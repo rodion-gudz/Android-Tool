bf5752861388f201f4b68fb44a4360fd9549b393
fast-geek <rodiongudz.services@gmail.com>
2021-04-10 14:50:33 +0300

    Program version update

--- src/AndroidTool.kt
+++ src/AndroidTool.kt
@@ -89,7 +89,7 @@ val userFolder = System.getProperty("user.home").toString()
 var SdkDir = userFolder + if (Windows) { "\\.android_tool\\SDK-Tools\\"} else if (Linux) { "/.android_tool/SDK-Tools/" } else { "/.android_tool/SDK-Tools/"}
 val ProgramDir = userFolder + if (Windows) { "\\.android_tool\\"} else if (Linux) { "/.android_tool/" } else { "/.android_tool/"}
 val programBuildDate = getProgramBuildTime()
-const val programVersion = "1.2.1"
+const val programVersion = "1.2.3"
 var programVersionLatest = programVersion
 val appProp = Properties()
 
